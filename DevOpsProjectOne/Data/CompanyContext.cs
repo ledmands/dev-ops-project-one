ea3085db57f7e9eb61e40ff043c67f084eba73bd
ledmands <ledmands+github@proton.me>
2023-02-10 08:27:03 -0500

    Added company context, I think this is the equivalent of Skynet in Azure. Added User.cs, cannot yet verify which table this would translate to, but I think it should be People or whichever table lists employees

---
+++ DevOpsProjectOne/Data/CompanyContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DevOpsProjectOne.Models;
+
+namespace DevOpsProjectOne.Data
+{
+    public class CompanyContext : DbContext
+    {
+        public CompanyContext (DbContextOptions<CompanyContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; } = default!;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("User");
+        }
+    }
+}
---
+++ DevOpsProjectOne/Pages/Users/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DevOpsProjectOne.Data;
+using DevOpsProjectOne.Models;
+
+namespace DevOpsProjectOne.Pages.Users
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly DevOpsProjectOne.Data.CompanyContext _context;
+
+        public DeleteModel(DevOpsProjectOne.Data.CompanyContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+      public User User { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Users == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.Users.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                User = user;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Users == null)
+            {
+                return NotFound();
+            }
+            var user = await _context.Users.FindAsync(id);
+
+            if (user != null)
+            {
+                User = user;
+                _context.Users.Remove(User);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
