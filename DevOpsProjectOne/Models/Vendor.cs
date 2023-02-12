dd0ec5d5e0b220bbfb0a2b36425a0e0b8fc47b45
ledmands <ledmands+github@proton.me>
2023-02-12 09:57:03 -0500

    Added the other tables to the Models folder along with their Fields

---
+++ DevOpsProjectOne/Models/Vendor.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevOpsProjectOne.Models
+{
+    public class Vendor
+    {
+        [Key]
+        public int Vendor_id { get; set; } // This will be the primary key. Entity automatically interprets ID as the primary key unless otherwise specified
+        public String Name { get; set; }
+        public String Location { get; set; }
+       
+    }
+}
